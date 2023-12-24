722a65e99541ba5a76295ab91729c74851eee319
dorianGT <doriangrouteaupro@gmail.com>
2023-12-24 09:41:28 +0100

    Update main -summary & split method
    Delete unnecessary file fauxmain
    Add commentary to main and split method

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import argparse
-import extension               
-
+import extension  
 
 def parse_arguments():
+    """Parse command line arguments.
+
+    Returns:
+        Namespace: Parsed arguments
+    """
     parser = argparse.ArgumentParser(description="Argumentation Framework Solver")
     parser.add_argument("-p", choices=["VE-CO", "VE-ST", "DC-CO", "DS-CO", "DC-ST", "DS-ST"],
                         help="Problem type")
@@ -10,27 +14,70 @@ def parse_arguments():
     parser.add_argument("-a", nargs="+", help="Names of arguments in the query set or query argument")
     return parser.parse_args()
 
-def load_data(file_path):
+def load_graph(file_path):
+    """Load the graph from a file.
+
+    Args:
+        file_path (str): Path to the text file describing the argumentation framework.
+    """
     extension.graphe.get_graph(file_path)
+
+def get_data():
+    """Perform main graph-related computations.
+
+    This function includes retrieving attack, defend, relation information,
+    checking conflict-free status, detecting cycles, and computing grounded,
+    complete, and stable extensions.
+
+    Note: This is the main logic for handling graph-related computations.
+    """
+
+    # Retrieve attack, defend, and relation information
     extension.graphe.get_attack()
     extension.graphe.get_defend()
     extension.graphe.get_relation()
+
+    # Check conflict-free status
     extension.graphe.conflict_free()
-    #admissible()
+
+    # Perform cycle detection for each argument
     for arg1 in extension.graphe.list_arg:
         extension.cycle(arg1, visited=None)
+
+    # Check if a cycle is detected
     if extension.graphe.is_cycle:
+        # Compute grounded, complete, and stable extensions if there is a cycle
         extension.grounded()
         extension.complete()
-        extension.stable()                                
+        extension.stable()
     else:
+        # Compute well-founded extension if there is no cycle
         extension.well_founded()
 
-def process_argument_VE(arg):
+
+
+def get_arguments_VE(arg):
+    """Get arguments for VE-CO and VE-ST problems.
+
+    Args:
+        arg (str): Argument string
+
+    Returns:
+        list: Processed arguments
+    """
     return [] if arg == "[]" else arg.upper().split(',')
 
-def process_argument_DCDS(arg):
+def get_argument_DCDS(arg):
+    """Get arguments for DC-CO, DS-CO, DC-ST, and DS-ST problems.
+
+    Args:
+        arg (str): Argument string
+
+    Returns:
+        str: Processed arguments
+    """
     return arg.upper()
+
 
 def main():
     global gr, st, pr, co, cf, ad, list_arg, dict_graph, dict_defend, dict_attack, dict_relation, is_cycle
@@ -39,7 +86,10 @@ def main():
     args = parse_arguments()
 
     # Load data from the file
-    load_data(args.f)
+    load_graph(args.f)
+
+    # Get all data for the graph
+    get_data()
 
     is_verify = False
 
@@ -59,13 +109,16 @@ def main():
     elif args.p == "DS-ST":
         extensions = extension.skep_stable(extension.graphe.st)
 
-    # Process the argument
-    arg = process_argument_VE(args.a[0]) if is_verify else process_argument_DCDS(args.a[0])
+    # Get the argument
+    arg = get_arguments_VE(args.a[0]) if is_verify else get_argument_DCDS(args.a[0])
 
+    # Convert the list of extensions to a list of sets
     list_set = []
     for ext in extensions:
         list_set.append(set(ext))
-    extensions = list_set        
+    extensions = list_set
+
+    # Convert the argument to a set for comparison
     arg = set(arg)
 
     # Check if the argument is in the extension
